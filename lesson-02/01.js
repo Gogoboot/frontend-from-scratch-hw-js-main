aedd11409a98d7927ed5518e87b0b2e9116f56a2
AndrewGogo <gogoboot@yandex>
2025-05-13 18:15:04 +0300

    lesson 3/1 js

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,26 +1,26 @@
 /*
-* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
-* Полученное булево значение сохраните в переменной isAccess.
+ * Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
+ * Полученное булево значение сохраните в переменной isAccess.
 
-* Условия доступа:
-* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
-* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
+ * Условия доступа:
+ * - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
+ * - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
 */
 
-// тестовые данные (значения можно менять)
-const isAdmin = true
-const isVerifiedUser = false
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+ // тестовые данные (значения можно менять)
+ const isAdmin = true
+ const isVerifiedUser = false
+ const hasSpecialPermission = true
+ const hasTemporaryPass = false
 
-let isAccess
+ let isAccess
 
-// your code
+ // your code
 
-if ((isAdmin === true || isVerifiedUser === true) && (hasSpecialPermission === true || hasTemporaryPass === true)) {
+ if ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)) {
     isAccess = true
-} else {
-    isAccess = false
-}
-// isAccess = false
-console.log(isAccess)
+ } else {
+     isAccess = false
+ }
+ // isAccess = false
+ console.log(isAccess)
--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,8 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(numberOne, numberTwo) {
+    let sumAverage =(numberOne + numberTwo)/2;
+    return sumAverage
+}
+
+console.log(calculateAverage(657, 0.765))
