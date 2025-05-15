8cd92fcadb486275f3adb7c2421e474c3d7585ff
AndrewGogo <gogoboot@yandex>
2025-05-15 04:58:52 +0300

    lesson 3/3 js

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,21 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(numberOne, numberTwo, numberThree) {
+    let largest = numberOne;
+    if (numberOne > numberTwo) {
+        largest = numberOne;
+    } else {
+        largest = numberTwo;
+    }
+
+    if (largest > numberThree) {
+        largest = largest;
+    } else {
+        largest = numberThree;
+    }
+
+    // console.log(largest);
+
+}
+
+// const result = findLargest(12, 30, 100);
