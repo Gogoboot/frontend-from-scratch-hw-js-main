021d82b58b289ec1b5136bf91860b10eea7023cd
AndrewGogo <gogoboot@yandex>
2026-01-28 02:00:15 +0300

    lesson-04/04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,23 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+// const word1 = "магия";
+// const word2 = "книга";
+
+// Если функция написана для массивов, просто превращаем строки в них:
+// const commonLetters = findCommonElements(word1.split(''), word2.split(''));
+// console.log(commonLetters); // ["г", "и", "а"]
+
+
+function doubleEachCharacter(word1) {
+    let result ='';
+
+    for (let i = 0; i < word1.length; i++) {
+
+        result  += word1[i] + word1[i];
+    }
+        return result;
+    }
+
+   // console.log(doubleEachCharacter('monitor'));
+
